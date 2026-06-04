cb797a860c21db50d83456ec69e1b68bdccae602
itssajitha <saji18920@gmail.com>
2026-06-04 17:14:21 +0530

    Q8.py

---
+++ work/Day3/Q8.py
@@ -0,0 +1,7 @@
+def largest(a, b, c):
+    return max(a, b, c)
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+print("Largest number is:", largest(a, b, c))
+
